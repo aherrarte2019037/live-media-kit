f93a7153ee347b969b764e993b3fdaadd42230ba
Angel <angelherrarte3@gmail.com>
2025-11-28 13:38:18 -0600

    Add types

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,63 +1,57 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export interface Database {
   public: {
     Tables: {
       profiles: {
         Row: {
-          id: string
-          email: string
-          username: string | null
-          tier: "free" | "pro"
-          onboarding_completed: boolean
-          created_at: string
-          updated_at: string
-        }
+          id: string;
+          email: string;
+          username: string | null;
+          tier: "free" | "pro";
+          onboarding_completed: boolean;
+          created_at: string;
+          updated_at: string;
+        };
         Insert: {
-          id: string
-          email: string
-          username?: string | null
-          tier?: "free" | "pro"
-          onboarding_completed?: boolean
-          created_at?: string
-          updated_at?: string
-        }
+          id: string;
+          email: string;
+          username?: string | null;
+          tier?: "free" | "pro";
+          onboarding_completed?: boolean;
+          created_at?: string;
+          updated_at?: string;
+        };
         Update: {
-          id?: string
-          email?: string
-          username?: string | null
-          tier?: "free" | "pro"
-          onboarding_completed?: boolean
-          created_at?: string
-          updated_at?: string
-        }
+          id?: string;
+          email?: string;
+          username?: string | null;
+          tier?: "free" | "pro";
+          onboarding_completed?: boolean;
+          created_at?: string;
+          updated_at?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "profiles_id_fkey"
-            columns: ["id"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
+            foreignKeyName: "profiles_id_fkey";
+            columns: ["id"];
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
